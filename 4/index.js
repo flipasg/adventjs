6b712d84e91738a2737a76dc12764de60b3948ae
Iker Garcia <flipas.garcia@gmail.com>
2021-12-04 18:05:29 +0100

    feat: add fourth exercise

---
+++ 4/index.js
@@ -0,0 +1,34 @@
+function createXmasTreeTop(width, treeHeight) {
+  const treeRowCharacters = new Array(width).fill('_');
+
+  return new Array(treeHeight)
+    .fill(treeRowCharacters)
+    .map((row, index) => {
+      for (let i = treeHeight - 1 - index; i < treeHeight + index; i++) {
+        row[i] = '*';
+      }
+      return row.join('');
+    })
+    .join('\n');
+}
+
+function createXmasTreeTrunk(width, treeHeight, trunkHeight) {
+  const trunkRowCharacters = new Array(width).fill('_');
+  trunkRowCharacters[treeHeight - 1] = '#';
+  const trunkRow = trunkRowCharacters.join('');
+
+  return new Array(trunkHeight).fill(trunkRow).join('\n');
+}
+function createXmasTree(height) {
+  if (height < 1 || height > 100) return '';
+
+  const width = height * 2 - 1;
+
+  return (
+    createXmasTreeTop(width, height) +
+    '\n' +
+    createXmasTreeTrunk(width, height, 2)
+  );
+}
+
+console.log(createXmasTree(2));
