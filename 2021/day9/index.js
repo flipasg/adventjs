d75bcf5354b5701b890d0cf2ce3c427435fbc86a
Iker Garcia <flipas.garcia@gmail.com>
2021-12-15 11:53:07 +0100

    refactor: improve performance day9 (@clebal)

--- 2021/day9/index.js
+++ 2021/day9/index.js
@@ -1,14 +1,10 @@
 export default function groupBy(collection, it) {
-  const groupedValues = {};
-  const mappedCollection = collection.map(
-    typeof it === 'function' ? it : (el) => el[it]
-  );
-  collection.forEach((el, index) => {
-    const key = mappedCollection[index];
+  return collection.reduce((groupedValues, el) => {
+    const key = typeof it === 'function' ? it(el) : el[it];
     if (!groupedValues[key]) {
       groupedValues[key] = [];
     }
     groupedValues[key].push(el);
-  });
-  return groupedValues;
+    return groupedValues;
+  }, {});
 }
